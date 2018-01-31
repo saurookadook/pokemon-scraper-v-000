6520f3fa1824fe8b0b064b78f3120820a16435a1
Andy Maskiell <maskiella@gmail.com>
2018-01-31 02:19:16 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -23,6 +23,7 @@ class Pokemon
   def self.find(id, db)
     # current_hp = db.execute("SELECT hp FROM pokemon WHERE id = ?", id)[0][0]
     # if current_hp
+    binding.pry
       pokemon_array = db.execute("SELECT * FROM pokemon WHERE id = ?", id)[0]
       self.new(id: pokemon_array[0], name: pokemon_array[1], type: pokemon_array[2], hp: current_hp, db: db)
     # else
