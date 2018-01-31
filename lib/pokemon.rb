a68bb4b9b65f2ea91dea788ed66e9d753a02563f
Andy Maskiell <maskiella@gmail.com>
2018-01-31 01:57:39 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -25,10 +25,10 @@ class Pokemon
     if current_hp
       pokemon_array = db.execute("SELECT * FROM pokemon WHERE id = ?", id)[0]
       self.new(id: pokemon_array[0], name: pokemon_array[1], type: pokemon_array[2], hp: current_hp, db: db)
-    # else
-    #   pokemon_array = db.execute("SELECT * FROM pokemon WHERE id = ?", id)[0]
-    #   self.new(id: pokemon_array[0], name: pokemon_array[1], type: pokemon_array[2], hp: 60, db: db)
-    # end
+    else
+      pokemon_array = db.execute("SELECT * FROM pokemon WHERE id = ?", id)[0]
+      self.new(id: pokemon_array[0], name: pokemon_array[1], type: pokemon_array[2], hp: 60, db: db)
+    end
   end
 
   def alter_hp(hp, db)
